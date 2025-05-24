b0de7963a1b72eccdddbf27ae63ab46ef3e38b47
dyfrigerio <dyfrigerio@AWSNV-L-VDA01.itbadc.itba.edu.ar>
2025-05-24 20:32:07 -0300

    Agrego un par de comentarios

--- main.c
+++ main.c
@@ -23,16 +23,18 @@
 #include "lib/nstdio/nstdio.h"
 #include "lib/port_emulator/port_emulator.h"
 
+// Maximum number of LEDs the program supports
 #define MAX_LEDS 16
 #define BUFFER 100
 
+// Enumeration to represent the user's input option
 enum
 {
-	LED_SELECTOR = 0,
-	NOT_LED,
-	OFF_ALL,
-	ON_ALL,
-	FINISH
+	LED_SELECTOR = 0,	// User entered a number to turn on a specific LED
+	NOT_LED,			// User chose to toggle all LEDs
+	OFF_ALL,			// User chose to turn all LEDs off
+	ON_ALL,				// User chose to turn all LEDs on
+	FINISH				 // User chose to quit the program
 };
 
 enum
@@ -41,8 +43,27 @@ enum
 	RUNNING,
 };
 
+/**
+ * @brief Displays the LED configuration menu and reads user input.
+ * @param ledCount Pointer to store the number of LEDs selected by the user.
+ * @param ledMask Pointer to store the initial bitmask of LEDs to turn on.
+ */
 void showMenu1(int *ledCount, uint32_t *ledMask);
+
+/**
+ * @brief Displays the LED control menu.
+ *
+ * @param ledCount Number of LEDs configured (used to display valid index range).
+ */
 void showMenu2(int ledCount);
+
+/**
+ * @brief Reads and interprets the user's menu selection.
+ *
+ * @param ledSelector Pointer to store the index of the LED selected by the user.
+ * @param ledCount Number of LEDs configured, used to validate index.
+ * @return One of the predefined menu actions (LED_SELECTOR, NOT_LED, OFF_ALL, ON_ALL, FINISH), or 0xFF if invalid.
+ */
 uint8_t readMenuOption(int *ledSelector, int ledCount);
 
 int main(void)
@@ -51,13 +72,18 @@ int main(void)
 	int ledCount;
 	uint32_t ledMaskOn;
 	uint32_t ledInitMask = 0;
+
 	showMenu1(&ledCount, &ledMaskOn);
+
+	// Initialize each LED pin and build the initialization mask
 	for(uint8_t i = 0; i < ledCount; i++)
 	{
 		ledInitMask <<= 1;
 		GPIO_PinInit(PORTD, i, OUTPUT);
 		ledInitMask += 1;
 	}
+
+	// Set initial LED states using the provided mask
 	GPIO_SetMaskedOutput(PORTD, ledMaskOn, HIGH);
 	display_leds(ledCount, ledMaskOn);
 
@@ -130,6 +156,7 @@ void showMenu1(int *ledCount, uint32_t *ledMask)
 
 		uint16_t allowedMask = (1 << *ledCount) - 1;
 
+		// Check that the user's mask doesn't exceed allowed LEDs
 		if ((*ledMask & ~allowedMask) != 0)
 		{
 			printf("\t\t\t[ERROR] Mask contains bits outside the range of %d LEDs.\n", *ledCount);
@@ -159,6 +186,7 @@ uint8_t readMenuOption(int * ledSelector, int ledCount)
 	if(isDigit(ch[0]))
 	{
 		uint8_t i;
+		// Read all numeric digits
 		for(i = 0; isDigit(ch[i]); i++, ch[i] = getchar())
 		{
 			*ledSelector = (*ledSelector) * 10 + TODIGIT(ch[i]);
@@ -168,6 +196,7 @@ uint8_t readMenuOption(int * ledSelector, int ledCount)
 			ClearInputBuffer();
 		}
 
+		// Validate LED index
 		if (*ledSelector >= 0 && *ledSelector < ledCount)
 		{
 			return LED_SELECTOR;
@@ -178,6 +207,8 @@ uint8_t readMenuOption(int * ledSelector, int ledCount)
 			return 0xFF;
 		}
 	}
+
+	// If not a digit, convert to lowercase and check command
 	ch[0] = TOLOWER(ch[0]);
 	ClearInputBuffer();
 	switch(ch[0])
@@ -191,6 +222,6 @@ uint8_t readMenuOption(int * ledSelector, int ledCount)
 		case 'q':
 			return FINISH;
 		default:
-			return 0xFF;
+			return 0xFF;	 // Invalid character
 	}
 }
